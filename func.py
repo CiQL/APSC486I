8e1d2316b05116e4588ef72a65ba5c69d06352c1
CiQL <vn@ciql.net>
2018-11-04 20:01:23 -1000

    Functional version update

--- func.py
+++ func.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 # functional version
 import functools
+import random
 
 def Analyze(data):
-    pass
+    return _sum(data), _count(data), _avg(data)
+
+def _sum(data):
+    return data[0] + _sum(data[1:])
+
+def _avg(data):
+    return _sum(data) / _count(data)
+
+def _count(data):
+    if not data:
+        return 0
+    else:
+        return _count(data[1:]) + 1
 
 if __name__ == "__main__":
-    pass
+    data = [random.random() for _ in range(100000)]
+    result = Analyze(data)
+    print(result)
