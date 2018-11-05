668a2c34ca3acdc646cffc5954f15b91bab77429
CiQL <vn@ciql.net>
2018-11-04 22:37:22 -1000

    Started work on js versions

--- func.js
+++ func.js
@@ -1,5 +1,9 @@
 function Analyze(data) {
-    return
+    return [_sum(data), _avg(data), _mdn(data), _mode(data)]
 }
 
+function 
+
 Array.prototype.flatten = (arr) => arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flatten(val)) : acc.concat(val), [])
+
+console.log(Analyze(data))
--- oo.js
+++ oo.js
@@ -2,4 +2,21 @@ class DataAnalysis {
     constructor(data) {
         this.data = data
     }
+    analyze() {
+	return [this.sum(), this.average(), this.median(), this.mode()]
+    }
+    sum() {
+
+    }
+    average() {
+
+    }
+    median() {
+
+    }
+    mode() {
+
+    }
 }
+
+console.log(DataAnalysis(data).analyze())
