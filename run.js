6ee7d5e9f3cd24da94757dd2ea9e5fc8a62c33cc
CiQL <vn@ciql.net>
2018-11-26 10:47:14 -1000

    Remove run files

--- run.js
+++
@@ -1,6 +0,0 @@
-import * as oo from "oo"
-import * as func from "func"
-
-console.log("Running object-oriented...")
-
-console.log("Running functional...")
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# https://unix.stackexchange.com/a/414770
-psrecord $(pgrep proc-name1) --interval 1 --duration 60 --plot plot1.png &
-P1=$!
-psrecord $(pgrep proc-name2) --interval 1 --duration 60 --plot plot2.png &
-P2=$!
-wait $P1 $P2
-echo 'Done'
