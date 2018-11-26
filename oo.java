b04fba8517de8fee4b655ce58b725f3770731f09
CiQL <vn@ciql.net>
2018-11-26 10:45:33 -1000

    java

--- oo.java
+++ oo.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 class oo {
     private int[] data;
     oo(int[] data) {
         this.data = data;
     }
+    
     public int sum() {
 	int s = 0;
 	for (int i: this.data) {
@@ -10,10 +14,25 @@ class oo {
 	}
 	return s;
     }
-    public int avg() {
+    public double avg() {
 	return this.sum() / this.data.length;
     }
-    public String toString() {
-        return this.data;
+    public int median() {
+	int mdn = data.length / 2;
+	int[] array = data;
+	Arrays.sort(array);
+	if (mdn % 2 == 1) {
+	    return array[mdn];
+	}
+	return (array[mdn] + array[mdn+1]) / 2l
+    }
+    public int mode() {
+
+    }
+    public static void main() {
+	Random r = new Random();
+	int[] dataset = r.ints(0,10).limit(10000).toArray();
+	oo dataAnalysis = new oo(dataset);
+	System.out.println(dataAnalysis(dataset));
     }
 }
