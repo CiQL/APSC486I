dd26c14c63eca24a2e9dce699bc20d64f7a9d15a
CiQL <vn@ciql.net>
2018-11-28 23:40:07 -1000

    Completed code

--- func.cpp
+++ func.cpp
@@ -1,13 +1,52 @@
 #include <random>
 #include <algorithm>
 #include <iostream>
-#include <vector>
-static std::vector<int> analyze(int data[]) {
-	return std::vector<int>();
+#include <array>
+using std::array;
+
+constexpr int dataCount = 10000;
+
+static int sum(const array<int, dataCount> &data) {
+	return std::accumulate(data.begin(), data.end(), 0);
 }
+
+static int avg(const array<int, dataCount> &data) {
+	return sum(data) / dataCount;
+}
+
+static int median(const array<int, dataCount> &data) {
+	auto d = data;
+	std::sort(d.begin(), d.end());
+	if (dataCount%2 == 1) {
+		return d[dataCount/2];
+	}
+	return (d[dataCount/2] + d[(dataCount+1)/2]) / 2;
+}
+
+static int mode(const array<int, dataCount> &data) {
+	return std::accumulate(data.begin(), data.end(), 0, [&](int a, int b) { 
+		return (std::count(data.begin(), data.end(), a) > std::count(data.begin(), data.end(), b)) ? a : b;
+	});
+
+}
+
+static array<int, 4> analyze(const array<int, dataCount> &data) {
+	return array<int, 4>{sum(data), avg(data), median(data), mode(data)};
+}
+
+static void print(int &i) {
+	std::cout << i << '\n';
+}
+
 int main(int, char *[]) {
-	int data[10000];
-	std::generate(std::begin(data), std::end(data), rand() % 10);
-	std::cout << analyze(data) << "\n";
+	std::random_device rd;
+	std::mt19937_64 mt{rd()};
+	std::uniform_int_distribution<> dist{0, 10};
+
+	array<int, dataCount> data{};
+	std::generate(std::begin(data), std::end(data), [&](){ return dist(mt); });
+
+	auto a = analyze(data);
+	std::for_each(a.begin(), a.end(), print);
 	return 0;
 }
--- oo.cpp
+++ oo.cpp
@@ -1,24 +1,68 @@
 #include <random>
 #include <algorithm>
 #include <iostream>
-#include <iterator>
+#include <array>
+#include <unordered_map>
+using std::array;
 
+constexpr int dataCount = 10000;
 class DataAnalysis {
 	private:
-		int data[];
+		array<int, dataCount> data;
 	public:
-	DataAnalysis(int d[]) {
-		data = d;
+	DataAnalysis(const array<int, dataCount> &d): data(d)
+	{}
+	int sum() {
+		int sum = 0;
+		for (int i = 0; i < dataCount; ++i) {
+			sum += data[i];
+		}
+		return sum;
 	}
-	int analyze() {
-		return;
+	int avg() {
+		return sum() / dataCount;
 	}
+	int median() {
+		auto d = data;
+		std::sort(d.begin(), d.end());
+		if (dataCount % 2 == 1) {
+			return d[dataCount / 2];
+		}
+		return (d[dataCount/2] + d[(dataCount+1)/2]) / 2;
+	}
+	int mode() {
+		array<int, dataCount> counts {0};
+		std::for_each(data.begin(), data.end(), [&](int i) { counts[i] += 1; });
+		int max = 0;
+		for (int i = 0; i < dataCount; ++i) {
+			if (counts[i] > counts[max]) {
+				max = i;
+			}
+		}
+		return max;
+	}
+	array<int, 4> analyze() {
+		return {sum(), avg(), median(), mode()};
+	}
+
 };
 
+void print(int &i) {
+	std::cout << i << '\n';
+}
+
 int main(int, char *[]) {
-	int data[10000];
-	std::generate(std::begin(data), std::end(data), rand() % 10);
-	DataAnalysis d = new DataAnalysis(data);
-	std::cout << d.analyze() << "\n";
+	std::random_device rd;
+	std::mt19937_64 mt{rd()};
+	std::uniform_int_distribution<> dist{0, 10};
+
+	array<int, dataCount> data{};
+	std::generate(data.begin(), data.end(), [&](){ return dist(mt); });
+
+	DataAnalysis d{data};
+
+	auto a = d.analyze();
+
+	std::for_each(a.begin(), a.end(), print);
 	return 0;
 }
